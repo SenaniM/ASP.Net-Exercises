f0f60d7dc1cdd70c76f5f9bd1f4a2cd9b7c41c0d
SenaniM <maheshika@hsenidlanka.com>
2017-08-20 15:07:29 +0530

    Committed Mydetails app using MVC

---
+++ MyDetails/MyDetails/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyDetails.Controllers
+{
+    public class HomeController : Controller
+    {
+       public String name = "Maheshika Weerasinghe";
+       public String city = "Kandy";
+       public String email = "maheshika@hsenidlanka.com";
+       public String desg = "Software Engineer";
+       public String company = "hSenid Software International Pvt.";
+        public string add = "No. 1/370, Ward Place, Colombo 07 ";
+        public string telephone = "011254789";
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Bio()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            ViewData["myname"] = name;
+            ViewData["mycity"] = city;
+            ViewData["myemail"] = email;
+            ViewData["myocc"] = desg;
+            ViewData["mycompany"] = company;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Please refere following ....";
+
+            Models.Contacts cont = new Models.Contacts();
+            cont.setName(name);
+            cont.setDesignation(desg);
+            cont.setCompany(company);
+            cont.setAddress(add);
+            cont.setPhone(telephone);
+            cont.setEmail(email);
+
+            return View(cont);
+        }
+    }
+}
---
+++ MyDetails/MyDetails/Models/Contacts.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyDetails.Models
+{
+    public class Contacts
+    {
+        public string name,
+                      company,
+                      designation,
+                      address,
+                      email,
+                      telephone;
+
+        public void setName(string Name)
+        {
+            this.name = Name;
+        }
+        public string getName()
+        {
+            return name;
+        }
+
+        public void setCompany(string Company)
+        {
+            this.company = Company;
+        }
+        public string getCompany()
+        {
+            return company;
+        }
+
+        public void setDesignation(string ds)
+        {
+            this.designation = ds;
+        }
+        public string getDesig()
+        {
+            return designation;
+        }
+
+        public void setAddress(string add)
+        {
+            this.address = add;
+        }
+        public string getAddress()
+        {
+            return address;
+        }
+
+        public void setPhone(string phone)
+        {
+            this.telephone = phone;
+        }
+        public string getPhone()
+        {
+            return telephone;
+        }
+
+        public void setEmail(string email)
+        {
+            this.email = email;
+        }
+        public string getEmail()
+        {
+            return email;
+        }
+
+    }
+}
